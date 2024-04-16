4cf499f1716d4896e972b95f18a7e3748ced348e
Mehrdad Mehralian <dadwic0@gmail.com>
2024-04-16 22:18:32 +0300

    fix: rename rialir to isturkiye

--- ios/isturkiyeTests/rialirTests.m
+++ ios/isturkiyeTests/rialirTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface rialirTests : XCTestCase
+@interface isturkiyeTests : XCTestCase
 
 @end
 
-@implementation rialirTests
+@implementation isturkiyeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {

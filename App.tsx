fe14ec4ee5dd8f3050ed4a22c8c099cdc2699a7e
Mehrdad Mehralian <dadwic0@gmail.com>
2024-04-20 23:25:36 +0300

    reinstall react-native-safe-area-context

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {useColorScheme} from 'react-native';
+import {useColorScheme, StyleSheet} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {ThemeMode, ThemeProvider, darkColors, lightColors} from '@rneui/themed';
 import {
   NavigationContainer,
@@ -15,18 +16,26 @@ function App(): React.JSX.Element {
   const navigationRef = useNavigationContainerRef();
 
   return (
-    <NavigationContainer
-      ref={navigationRef}
-      onReady={() => {
-        routeNameRef.current = navigationRef.getCurrentRoute()?.name;
-      }}
-      theme={scheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <ThemeProvider
-        theme={{mode: scheme as ThemeMode, darkColors, lightColors}}>
-        <AppLayout />
-      </ThemeProvider>
-    </NavigationContainer>
+    <SafeAreaProvider style={styles.container}>
+      <NavigationContainer
+        ref={navigationRef}
+        onReady={() => {
+          routeNameRef.current = navigationRef.getCurrentRoute()?.name;
+        }}
+        theme={scheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <ThemeProvider
+          theme={{mode: scheme as ThemeMode, darkColors, lightColors}}>
+          <AppLayout />
+        </ThemeProvider>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;

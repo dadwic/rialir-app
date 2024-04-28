ec2073c71da15dbfd1fb27ca2d36c32c24ae6b6a
Mehrdad Mehralian <dadwic0@gmail.com>
2024-04-28 20:44:49 +0300

    remove useNavigationContainerRef, install netinfo

--- App.tsx
+++ App.tsx
@@ -6,23 +6,14 @@ import {
   NavigationContainer,
   DefaultTheme,
   DarkTheme,
-  useNavigationContainerRef,
 } from '@react-navigation/native';
 import AppLayout from './src/AppLayout';
 
 function App(): React.JSX.Element {
   const scheme = useColorScheme();
-  const routeNameRef = React.useRef<string>();
-  const navigationRef = useNavigationContainerRef();
-
   return (
     <SafeAreaProvider style={styles.container}>
-      <NavigationContainer
-        ref={navigationRef}
-        onReady={() => {
-          routeNameRef.current = navigationRef.getCurrentRoute()?.name;
-        }}
-        theme={scheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <NavigationContainer theme={scheme === 'dark' ? DarkTheme : DefaultTheme}>
         <ThemeProvider
           theme={{mode: scheme as ThemeMode, darkColors, lightColors}}>
           <AppLayout />

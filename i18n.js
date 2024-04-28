3d7e7a9e2833439d4c5f204e1030a4b6ba7c4c91
Mehrdad Mehralian <dadwic0@gmail.com>
2024-04-28 19:30:45 +0300

    change fallbackLng

--- i18n.js
+++ i18n.js
@@ -5,7 +5,7 @@ import enTranslation from './locales/en/translation.json';
 import faTranslation from './locales/fa/translation.json';
 
 i18n.use(initReactI18next).init({
-  fallbackLng: 'en',
+  fallbackLng: 'fa',
   supportedLngs: ['en', 'fa'],
   resources: {
     en: {

ba4d559facc2404668f66002aab0d0235afdd922
Mehrdad Mehralian <dadwic0@gmail.com>
2024-04-28 18:39:06 +0300

    remove react-native-localization-settings

--- i18n.js
+++ i18n.js
@@ -1,22 +1,18 @@
 import 'intl-pluralrules';
 import i18n from 'i18next';
 import {initReactI18next} from 'react-i18next';
-import {ReactNativeLanguageDetector} from 'react-native-localization-settings';
 import enTranslation from './locales/en/translation.json';
 import faTranslation from './locales/fa/translation.json';
 
-i18n
-  .use(ReactNativeLanguageDetector)
-  .use(initReactI18next)
-  .init({
-    fallbackLng: 'en',
-    supportedLngs: ['en', 'fa'],
-    resources: {
-      en: {
-        translation: enTranslation,
-      },
-      fa: {
-        translation: faTranslation,
-      },
+i18n.use(initReactI18next).init({
+  fallbackLng: 'en',
+  supportedLngs: ['en', 'fa'],
+  resources: {
+    en: {
+      translation: enTranslation,
     },
-  });
+    fa: {
+      translation: faTranslation,
+    },
+  },
+});

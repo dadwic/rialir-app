20699cbccf507e08679bd91a877a1f3a893fcad1
Mehrdad Mehralian <dadwic0@gmail.com>
2024-05-04 02:12:40 +0300

    force rtl using I18nManager

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, I18nManager} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import './i18n';
+
+I18nManager.allowRTL(true);
+I18nManager.forceRTL(true);
 
 AppRegistry.registerComponent(appName, () => App);

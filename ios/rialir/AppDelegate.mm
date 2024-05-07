a01a9a55e73daafc22b5ebbce9dec09800f15365
Mehrdad Mehralian <dadwic@yahoo.com>
2024-05-08 01:00:47 +0300

    update AppDelegate.mm add allowRTL

--- ios/rialir/AppDelegate.mm
+++ ios/rialir/AppDelegate.mm
@@ -1,5 +1,6 @@
 #import "AppDelegate.h"
 
+#import <React/RCTI18nUtil.h>
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
@@ -10,6 +11,8 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+
+  [[RCTI18nUtil sharedInstance] allowRTL:YES];
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }

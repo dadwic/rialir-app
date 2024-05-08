d2c9db9666ae02e508207424583651424796d6ee
Mehrdad Mehralian <dadwic@yahoo.com>
2024-05-08 20:30:15 +0300

    add android app locale

--- index.js
+++ index.js
@@ -2,11 +2,9 @@
  * @format
  */
 
-import {AppRegistry, I18nManager} from 'react-native';
+import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import './i18n';
-
-I18nManager.allowRTL(true);
 
 AppRegistry.registerComponent(appName, () => App);
